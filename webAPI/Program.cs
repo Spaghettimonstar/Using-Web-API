ce41b8adfb93f9b8ce82ca44bb2b7dcc73242792
SpaghettiMonster <norbybutora123@gmail.com>
2024-04-19 11:35:03 -0400

    Added RestSharp to code, started to work on Requesting from API

---
+++ webAPI/Program.cs
@@ -0,0 +1,15 @@
+﻿using RestSharp;
+using RestSharp.Authenticators; 
+using System;
+class Program {
+  static void Main() {
+    string url = ""; {
+url = "https://www.cheapshark.com/api/1.0/games?title=batman";
+
+
+    }
+    var request = new RestSharp.Request(url);
+    Console.WriteLine("Hello World");
+    Console.WriteLine("Test");
+  }
+}
