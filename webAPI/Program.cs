e226b8dcd2f66906ccd60454193198f4353e945a
SpaghettiMonster <norbybutora123@gmail.com>
2024-04-23 11:35:15 -0400

    Attempted to deserialize, no luck

--- webAPI/Program.cs
+++ webAPI/Program.cs
@@ -33,16 +33,29 @@ url = "https://www.cheapshark.com/api/1.0/games?title=Dragons Dogma";
 
 
     }
+    string jsonString =  {"internalName": "DEUSEXHUMANREVOLUTIONDIRECTORSCUT", "title": "Deus Ex: Human Revolution - Director's Cut", "metacriticLink":/game/pc/deus-ex-human-revolution---directors-cut", "dealID": "HhzMJAgQYGZ%2B%2BFPpBG%2BRFcuUQZJO3KXvlnyYYGwGUfU%3D", "storeID": "1", "gameID": "102249", "salePrice": "2.99", "normalPrice": "19.99", "isOnSale": "1", "savings": "85.042521", "metacriticScore": "91", "steamRatingText": "Very Positive",
+    "steamRatingPercent": "92",
+    "steamRatingCount": "17993",
+    "steamAppID": "238010",
+    "releaseDate": 1382400000,
+    "lastChange": 1621536418,
+    "dealRating": "9.6",
+    "thumb": "https://cdn.cloudflare.steamstatic.com/steam/apps/238010/capsule_sm_120.jpg?t=1619788192" 
+    }
+
+    Stuff stuff =JsonSerializer.Deserialize<Stuff>(jsonString);
+Console.WriteLine($"title: {Stuff?.title}");
+    
     
     var client =  new RestClient(url);
-// var id = "";
+var title = "";
     var request = new RestRequest();
     var response = client.Get(request);
 
     Console.WriteLine(response.Content.ToString());
 
-// if (id == "Lethal Company") {
-//   Console.WriteLine("Cheapest price; " + cheapest);
+// if (title == "Lethal Company") {
+//   Console.WriteLine("Cheapest price; " + salePrice);
 // }
     
   }
